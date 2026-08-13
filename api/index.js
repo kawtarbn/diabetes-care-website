052132987addfae47e880d399facf2a00cbe3866
kawtarbenabdelmoumene-ai <kawtarbenabdelmoumene@gmail.com>
2026-08-13 23:20:51 +0200

    Simplify API for Vercel deployment testing

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
-const app = require('../server');
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const app = express();
 
-module.exports = (req, res) => {
-    app(req, res);
-};
+// Basic middleware
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+// Health check
+app.get('/', (req, res) => {
+    res.json({ status: 'ok', message: 'API is running' });
+});
+
+module.exports = app;
