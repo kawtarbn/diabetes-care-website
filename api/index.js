8db322da69ff9c0278e2391d8abdd5c76430de2c
kawtarbenabdelmoumene-ai <kawtarbenabdelmoumene@gmail.com>
2026-08-13 23:24:13 +0200

    Remove vercel.json and simplify to basic serverless function

--- api/index.js
+++ api/index.js
@@ -1,16 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const helmet = require('helmet');
-const app = express();
-
-// Basic middleware
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
-
-// Health check
-app.get('/', (req, res) => {
+module.exports = (req, res) => {
     res.json({ status: 'ok', message: 'API is running' });
-});
-
-module.exports = app;
+};
